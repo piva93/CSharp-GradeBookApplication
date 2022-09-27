66671b71ac3a4cf28f4990662b1b23aec69c95d4
piva93 <benedettapiva@biosero.com>
2022-09-27 13:40:58 +0200

    pluralsight1

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+
+    }
+}
