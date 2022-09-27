33bda282490bc0cc3ab5d1588249b1995524af2a
piva93 <benedettapiva@biosero.com>
2022-09-27 13:55:21 +0200

    pr4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,23 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new ArgumentException("Ranked-grading requires a minimum of 5 studetns to work");
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
     }
 }
