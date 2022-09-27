31f0c7cc15630d56701daaee60c2861016914655
piva93 <benedettapiva@biosero.com>
2022-09-27 14:52:52 +0200

    PR13

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace GradeBook.GradeBooks
 {
@@ -112,21 +113,51 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            if (IsWeighted)
+            var gpa = 0;
+            
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                switch (letterGrade)
+                {
+                    case 'A':
+                        gpa = 5;
+                        break;
+                    case 'B':
+                        gpa = 4;
+                        break;
+                    case 'C':
+                        gpa = 3;
+                        break;
+                    case 'D':
+                        gpa = 2;
+                        break;
+                    case 'F':
+                        gpa = 1;
+                        break;
+                }
+
+            }
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
